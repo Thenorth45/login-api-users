5f7f0476f478d8d41719497f1f879e1f3643ccfa
Thenorth45 <pnotkioh@gmail.com>
2024-08-10 19:36:47 +0700

    second

--- routes/boxerdata.js
+++ routes/boxerdata.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 router.use(cors());
 
+// GET all boxers
 router.get('/', async (req, res, next) => {
   try {
     const boxerdata = await Boxer.find();
@@ -15,10 +16,39 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// POST a new boxer
 router.post('/', async (req, res, next) => {
   try {
     const post = await Boxer.create(req.body);
     res.json(post);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE a boxer by ID
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedBoxer = await Boxer.findByIdAndDelete(id);
+    if (!deletedBoxer) {
+      return res.status(404).json({ message: 'Boxer not found' });
+    }
+    res.json({ message: 'Boxer deleted successfully', deletedBoxer });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// UPDATE a boxer by ID
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updatedBoxer = await Boxer.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedBoxer) {
+      return res.status(404).json({ message: 'Boxer not found' });
+    }
+    res.json({ message: 'Boxer updated successfully', updatedBoxer });
   } catch (err) {
     next(err);
   }
